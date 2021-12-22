9adf2687219a3adbc71a597c8311aabbe9e78a9b
fastkylin <fastkylin@hotmail.com>
2021-12-23 01:38:51 +0800

    format source code and update .gitignore

--- 1114_Foo.cpp
+++ 1114_Foo.cpp
@@ -2,60 +2,61 @@
 #include <functional>
 #include <future>
 #include <iostream>
-#include <thread>
-#include <vector>
+
 class Foo {
 private:
-  std::mutex mMutex;
-  std::condition_variable mConditionVariable;
-  int mFlag;
+    std::mutex mMutex;
+    std::condition_variable mConditionVariable;
+    int mFlag;
 
 public:
-  Foo() { mFlag = 0; }
-  void first(std::function<void()> printFirst) {
-    std::unique_lock<std::mutex> lock(mMutex);
-    mConditionVariable.wait(lock, [=]() { return (0 == mFlag); });
-    // printFirst() outputs "first". Do not change or remove this line.
-    printFirst();
-    mFlag = 1;
-    lock.unlock();
-    mConditionVariable.notify_all();
-  }
+    Foo() { mFlag = 0; }
 
-  void second(std::function<void()> printSecond) {
-    std::unique_lock<std::mutex> lock(mMutex);
-    mConditionVariable.wait(lock, [=]() { return (1 == mFlag); });
-    // printSecond() outputs "second". Do not change or remove this line.
-    printSecond();
-    mFlag = 2;
-    lock.unlock();
-    mConditionVariable.notify_all();
-  }
+    void first(std::function<void()> printFirst) {
+        std::unique_lock<std::mutex> lock(mMutex);
+        mConditionVariable.wait(lock, [=]() { return (0 == mFlag); });
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        mFlag = 1;
+        lock.unlock();
+        mConditionVariable.notify_all();
+    }
 
-  void third(std::function<void()> printThird) {
-    std::unique_lock<std::mutex> lock(mMutex);
-    mConditionVariable.wait(lock, [=]() { return (2 == mFlag); });
-    // printThird() outputs "third". Do not change or remove this line.
-    printThird();
-    mFlag = 0;
-    lock.unlock();
-    mConditionVariable.notify_all();
-  }
+    void second(std::function<void()> printSecond) {
+        std::unique_lock<std::mutex> lock(mMutex);
+        mConditionVariable.wait(lock, [=]() { return (1 == mFlag); });
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        mFlag = 2;
+        lock.unlock();
+        mConditionVariable.notify_all();
+    }
+
+    void third(std::function<void()> printThird) {
+        std::unique_lock<std::mutex> lock(mMutex);
+        mConditionVariable.wait(lock, [=]() { return (2 == mFlag); });
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+        mFlag = 0;
+        lock.unlock();
+        mConditionVariable.notify_all();
+    }
 };
+
 int main(int argc, char *argv[]) {
-  std::function<void()> printFirst = []() { std::cout << "first\t"; };
-  std::function<void()> printSecond = []() { std::cout << "second\t"; };
-  std::function<void()> printThird = []() { std::cout << "third\t"; };
+    std::function<void()> printFirst = []() { std::cout << "first\t"; };
+    std::function<void()> printSecond = []() { std::cout << "second\t"; };
+    std::function<void()> printThird = []() { std::cout << "third\t"; };
 
-  Foo foo;
+    Foo foo;
 
-  auto a1 = std::async(&Foo::first, &foo, printFirst);
-  auto a2 = std::async(&Foo::second, &foo, printSecond);
-  auto a3 = std::async(&Foo::third, &foo, printThird);
+    auto a1 = std::async(&Foo::first, &foo, printFirst);
+    auto a2 = std::async(&Foo::second, &foo, printSecond);
+    auto a3 = std::async(&Foo::third, &foo, printThird);
 
-  a3.wait();
-  a2.wait();
-  a1.wait();
+    a3.wait();
+    a2.wait();
+    a1.wait();
 
-  return 0;
+    return 0;
 }
--- 1115_FooBar.cpp
+++ 1115_FooBar.cpp
@@ -2,60 +2,59 @@
 #include <functional>
 #include <future>
 #include <iostream>
-#include <thread>
-#include <vector>
 
 class FooBar {
 private:
-  int n;
-  std::mutex mMutex;
-  std::condition_variable mConditionVariable;
-  bool flag;
+    int n;
+    std::mutex mMutex;
+    std::condition_variable mConditionVariable;
+    bool flag;
 
 public:
-  FooBar(int n) {
-    this->n = n;
-    flag = true;
-  }
-
-  void foo(std::function<void()> printFoo) {
-
-    for (int i = 0; i < n; i++) {
-      std::unique_lock<std::mutex> lock(mMutex);
-      mConditionVariable.wait(lock, [=]() { return flag; });
-      // printFoo() outputs "foo". Do not change or remove this line.
-      printFoo();
-      flag = false;
-      lock.unlock();
-      mConditionVariable.notify_one();
+    FooBar(int n) {
+        this->n = n;
+        flag = true;
     }
-  }
 
-  void bar(std::function<void()> printBar) {
+    void foo(std::function<void()> printFoo) {
 
-    for (int i = 0; i < n; i++) {
-      std::unique_lock<std::mutex> lock(mMutex);
-      mConditionVariable.wait(lock, [=]() { return (!flag); });
-      // printBar() outputs "bar". Do not change or remove this line.
-      printBar();
-      flag = true;
-      lock.unlock();
-      mConditionVariable.notify_one();
+        for (int i = 0; i < n; i++) {
+            std::unique_lock<std::mutex> lock(mMutex);
+            mConditionVariable.wait(lock, [=]() { return flag; });
+            // printFoo() outputs "foo". Do not change or remove this line.
+            printFoo();
+            flag = false;
+            lock.unlock();
+            mConditionVariable.notify_one();
+        }
     }
-  }
+
+    void bar(std::function<void()> printBar) {
+
+        for (int i = 0; i < n; i++) {
+            std::unique_lock<std::mutex> lock(mMutex);
+            mConditionVariable.wait(lock, [=]() { return (!flag); });
+            // printBar() outputs "bar". Do not change or remove this line.
+            printBar();
+            flag = true;
+            lock.unlock();
+            mConditionVariable.notify_one();
+        }
+    }
 };
+
 int main(int argc, char *argv[]) {
 
-  std::function<void()> printFoo = []() { std::cout << "foo\t"; };
-  std::function<void()> printBar = []() { std::cout << "bar\t"; };
+    std::function<void()> printFoo = []() { std::cout << "foo\t"; };
+    std::function<void()> printBar = []() { std::cout << "bar\t"; };
 
-  FooBar foobar(2);
+    FooBar foobar(2);
 
-  auto threadOne = std::async(&FooBar::foo, &foobar, printFoo);
-  auto threadTwo = std::async(&FooBar::bar, &foobar, printBar);
+    auto threadOne = std::async(&FooBar::foo, &foobar, printFoo);
+    auto threadTwo = std::async(&FooBar::bar, &foobar, printBar);
 
-  threadOne.wait();
-  threadTwo.wait();
+    threadOne.wait();
+    threadTwo.wait();
 
-  return 0;
+    return 0;
 }
